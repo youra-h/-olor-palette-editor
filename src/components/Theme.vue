af9ffb2fb429fbb1a482fb291c1271909765a691
youra_halloween <bestletter@inbox.ru>
2023-09-20 21:14:22 +0500

    fix small change theme

--- src/components/Theme.vue
+++ src/components/Theme.vue
@@ -4,17 +4,35 @@
 
         <textarea id="colors-input" v-model.trim="text" class="form-control mb-2" rows="20" @input="onInput"></textarea>
 
-        <div v-for="(group, groupName) in theme.groups" :key="groupName">
+        <div v-for="(group, groupName) in theme.groups" :key="groupName" class="col-3">
             <h3>{{ groupName }}: {{ group.bg[0].value }}</h3>
 
             <div v-for="(onColor, index) in group.on" :key="'on-' + index">
-                <div class="bg p-2 mb-1" v-if="group.outline" v-for="(outlineColor, index) in group.outline" :key="'outline-' + index"
-                    :style="{ backgroundColor: group.bg[0].value, color: onColor.value, borderColor: outlineColor.value, borderStyle: 'solid', borderWidth: '1px'}">
-                    <div>Color: {{ onColor.value }}</div>
-                    <div>Outline: {{ outlineColor.value }}</div>
+                <div class="bg p-2 mb-1" v-if="group.outline" v-for="(outlineColor, index) in group.outline"
+                    :key="'outline-' + index"
+                    :style="{ backgroundColor: group.bg[0].value, color: onColor.value, borderColor: outlineColor.value, borderStyle: 'solid', borderWidth: '1px' }">
+                    <div class="color-text">
+                        <span>Color: {{ onColor.value }}</span>
+                        <span>
+                            <i class="fa fa-check p-1"></i>
+                            <i class="fa fa-times p-1"></i>
+                            <i class="fa fa-bell p-1"></i>
+                        </span>
+                    </div>
+                    <div class="outline">
+                        <span>Outline: {{ outlineColor.value }}</span>
+                        <span class="view" :style="{ backgroundColor: outlineColor.value }"></span>
+                    </div>
                 </div>
                 <div v-else class="bg p-2 mb-1" :style="{ backgroundColor: group.bg[0].value, color: onColor.value }">
-                    <div>Color: {{ onColor.value }}</div>
+                    <div class="color-text">
+                        <span>Color: {{ onColor.value }}</span>
+                        <span>
+                            <i class="fa fa-check p-1"></i>
+                            <i class="fa fa-times p-1"></i>
+                            <i class="fa fa-bell p-1"></i>
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
@@ -50,5 +68,18 @@ export default {
     font-family: monospace;
     font-size: .9rem;
     border-radius: 3px;
+}
+
+.color-text,
+.outline {
+    display: flex;
+    justify-content: space-between;
+}
+
+.outline .view {
+    display: inline-block;
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
 }
 </style>
